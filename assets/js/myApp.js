326381781b62af0d97a5c57362d372756c275776
June Choi <fanse2@gmail.com>
2022-01-12 12:22:03 -0800

    before change gall

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -3,6 +3,6 @@ var app = angular.module("myApp", []);
 app.controller("myCtrl", function ($scope, $http) {
   $http.get("info.json").then(function (response) {
     $scope.data = response.data;
-    //console.log($scope.data.rPhone);
+    console.log($scope.data);
   });
 });
