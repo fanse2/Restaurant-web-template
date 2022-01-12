55c75e1380b32cebcc0148354cc8a9e0997e4075
June Choi <fanse2@gmail.com>
2022-01-11 16:03:08 -0800

    angular

---
+++ assets/js/myApp.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function ($scope, $http) {
+  $http.get("info.json").then(function (response) {
+    $scope.data = response.data;
+    //console.log($scope.data.rPhone);
+  });
+});
